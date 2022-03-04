2bc26a337b23fc81fbd3d229b1c0f9a89eca9036
Enzo Noda <enzo.noda@gmail.com>
2022-03-03 22:05:08 -0300

    commit

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
+import { ITarefa } from '../../types/tarefa';
 import style from './Lista.module.scss'
 import Item from "./Item";
 
-function Lista() {
 
-  const [tarefas, setTarefas] = useState([{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  },{
-    tarefa: 'Typescript',
-    tempo: '01:00:00'
-  },{
-    tarefa: 'Javascript',
-    tempo: '00:30:00'
-  }])
+
+function Lista( {tarefas}: {tarefas: ITarefa[]}) {
+
+  
   return(
     <aside className={style.listaTarefas}>
-      <h2 onClick={()=>{
-        setTarefas([...tarefas, {tarefa: 'Estudar estado', tempo: '00:45:00'}])
-      }}>Estudos do dia</h2>
+      <h2 >Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index}{...item}/>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Cronometro from '../components/Cronometro';
 
+import Cronometro from '../components/Cronometro';
+import { ITarefa } from '../types/tarefa';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from  './App.module.scss'
+import { useState } from 'react';
 
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
   return (
     <div className={style.AppStyle}>
-      <Formulario/>
-      <Lista/>
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Cronometro/>
     </div>
   );
