9df9a153b75262254905b9047be56d2e15cc4310
Enzo Noda <enzo.noda@gmail.com>
2022-03-04 22:08:51 -0300

    commit

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,17 +2,21 @@ import { ITarefa } from '../../types/tarefa';
 import style from './Lista.module.scss'
 import Item from "./Item";
 
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa)=> void
+}
 
 
-function Lista( {tarefas}: {tarefas: ITarefa[]}) {
+function Lista( {tarefas, selecionaTarefa}: Props) {
 
   
   return(
     <aside className={style.listaTarefas}>
       <h2 >Estudos do dia</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index}{...item}/>
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id}{...item}/>
         ))}
       </ul>
     </aside>
